4520b3024d7396ca7e93a38f58549d489ce62f6e
Arun K <arthurbishop120@gmail.com>
2024-01-20 15:01:25 +0530

    size restriction changed any string can be size

--- pkg/domain/inventory.go
+++ pkg/domain/inventory.go
@@ -6,7 +6,7 @@ type Inventories struct {
 	Category    Category `json:"-" gorm:"foreignkey:CategoryID;constraint:OnDelete:CASCADE"`
 	ProductName string   `json:"product_name"`
 	Image       string   `json:"image"`
-	Size        string   `json:"size" gorm:"size:5;default:'M';check:size IN ('S', 'M', 'L', 'XL', 'XXL')"`
+	Size        string   `json:"size"`
 	Stock       int      `json:"stock"`
 	Price       float64  `json:"price"`
 }
